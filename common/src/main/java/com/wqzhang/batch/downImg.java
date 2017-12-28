8688a5d8a6827b5eee080696cd8c8c525f4e78f1
wqzhang <wqzhang0@163.com>
2017-12-28 16:14:52 +0800

    page-plugin/src/main/webapp/WEB-INF/jsp/upload/css/ 	page-plugin/src/main/webapp/WEB-INF/jsp/upload/images/default.bmp 	page-plugin/src/main/webapp/WEB-INF/jsp/upload/images/success.bmp 	page-plugin/src/main/webapp/WEB-INF/jsp/upload/images/uploadProccess.gif 	page-plugin/src/main/webapp/WEB-INF/jsp/upload/js/jquery-1.8.3.min.js 	page-plugin/src/main/webapp/WEB-INF/jsp/upload/js/jquery-labelauty.js 	page-plugin/src/main/webapp/WEB-INF/jsp/upload/upLoadImg.html 	page-plugin/src/main/webapp/WEB-INF/jsp/upload/upLoadImgTest.html

---
+++ common/src/main/java/com/wqzhang/batch/downImg.java
@@ -0,0 +1,11 @@
+package com.wqzhang.batch;
+
+/**
+ * @author wqzhang
+ * @version 1.0.0
+ * @ClassName downImg
+ * @Description ${todo}
+ * @Date 2017/12/13 10:26
+ */
+public class downImg {
+}
---
+++ page-plugin/src/main/java/com/wqzhang/controller/upload/UploadController.java
@@ -0,0 +1,11 @@
+package com.wqzhang.controller;
+
+/**
+ * @author wqzhang
+ * @version 1.0.0
+ * @ClassName upload
+ * @Description ${todo}
+ * @Date 2017/12/26 14:28
+ */
+public class upload {
+}
