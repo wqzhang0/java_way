7caabafe2fd15b6a2434c5a511e5c59d96e9a37b
wqzhang_home <wqzhang0@163.com>
2018-09-19 07:18:48 +0800

    add sort

--- ihadoop/src/main/java/com/wqzhang/order/GroupSort.java
+++ ihadoop/src/main/java/com/wqzhang/order/GroupSort.java
@@ -1,10 +1,17 @@
 package com.wqzhang.order;
 
+import com.wqzhang.flow.FlowBean;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 
@@ -13,16 +20,50 @@ import java.io.IOException;
  */
 public class GroupSort {
 
-    static class SortMapper extends Mapper<LongWritable,Text,OrderBean,NullWritable> {
+    static class SortMapper extends Mapper<LongWritable, Text, OrderBean, NullWritable> {
         OrderBean bean = new OrderBean();
 
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             String line = value.toString();
             String[] fields = line.split(",");
-            
-            bean.set(new Text(fields[0]),new DoubleWritable(Double.parseDouble(fields[2])));
-            context.write(bean,NullWritable.get());;
+
+            bean.set(new Text(fields[0]), new DoubleWritable(Double.parseDouble(fields[2])));
+            context.write(bean, NullWritable.get());
+            ;
+        }
+    }
+
+    static class SortReducer extends Reducer<OrderBean, NullWritable, OrderBean, NullWritable> {
+        @Override
+        protected void reduce(OrderBean key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
+            context.write(key, NullWritable.get());
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+        job.setJarByClass(GroupSort.class);
+
+        job.setOutputKeyClass(OrderBean.class);
+        job.setOutputValueClass(NullWritable.class);
+
+        job.setMapperClass(SortMapper.class);
+        job.setReducerClass(SortReducer.class);
+
+        job.setGroupingComparatorClass(MyGroupingComparator.class);
+        job.setNumReduceTasks(2);
+
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(FlowBean.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        boolean b = job.waitForCompletion(true);
+        if (!b) {
+            System.out.println("wordcount task fail!");
         }
     }
 }
